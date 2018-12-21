937bf71cc141be452dbd3887b1557835cb3eafae
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2018-12-21 19:21:22 +0100

    Working on running automated tests
    Guile was a dead end, but turns out we can just run emacs in batch
    mode and things work well :)

--- tests/guileTests.ts
+++ tests/guileTests.ts
@@ -1,7 +1,14 @@
 declare function message(msg: string): void
+declare function should(item: boolean): boolean
 
 function main() {
 	message("Foo bar")
 }
 
+//[form: ert-deftest]
+function testSomething() {
+	should(1 + 2 == 4)
+}
+
 main()
+testSomething()

6e0b15ac195e05d6ca0a5b4417216df4eaf5bc76
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2018-12-28 14:46:23 +0100

    Added a couple more tests

--- tests/src/tests.ts
+++ tests/src/tests.ts
@@ -2,14 +2,34 @@ import { should, equal } from 'ert'
 import * as emacs from 'emacs'
 
 //[Form: ert-deftest]
-function test1(){
+function testEquality(){
 	should(equal('a', 'a'))
 }
 
 //[Form: ert-deftest]
-function test2(){
+function testStringConcat(){
 	const a = 'a'
 	const b = 'b'
 	const ab = a + b
 	should(equal(ab, 'ab'))
+}
+
+//[Form: ert-deftest]
+function testObjectCreation(){
+	const obj = {
+		a: 'aaa',
+		b: 'bbb'
+	}
+	should(equal(obj.a, 'aaa'))
+	should(equal(obj.b, 'bbb'))
+}
+
+//[Form: ert-deftest]
+function testArrayCreation(){
+	const arr = [1,2,3,4,5]
+	should(equal(arr[0], 1))
+	should(equal(arr[1], 2))
+	should(equal(arr[2], 3))
+	should(equal(arr[3], 4))
+	should(equal(arr[4], 5))
 }

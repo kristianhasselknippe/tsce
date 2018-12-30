80e970a2e082c2e09f5150204a72c98b648d63c9
Haskellknippe <kristian_hasselknippe@outlook.com>
2018-12-31 00:18:45 +0100

    Fixed another quotation error

--- tsce/src/elispTypes/funcall.ts
+++ tsce/src/elispTypes/funcall.ts
@@ -9,8 +9,8 @@ export class FunctionCall extends Expression {
 		super();
 	}
 
-	emitArgs() {
-		return this.args.map(x => x.emit(0)).reduce((prev, curr) => prev + " " + curr, "")
+	emitArgs(indent: number, quoted = false) {
+		return this.args.map(x => quoted ? x.emitQuoted(0) : x.emit(0)).reduce((prev, curr) => prev + " " + curr, "")
 	}
 
 	funcallIfLambda() {
@@ -22,11 +22,11 @@ export class FunctionCall extends Expression {
 	}
 
 	emit(indent: number) {
-		return `${tabs(indent)}(${this.funcallIfLambda()}${this.leftHand.emit(0)} ${this.emitArgs()})`
+		return `${tabs(indent)}(${this.funcallIfLambda()}${this.leftHand.emit(0)} ${this.emitArgs(indent)})`
 	}
 
 	emitQuoted(indent: number) {
-		return `${tabs(indent)},(${this.funcallIfLambda()}${this.leftHand.emit(0)} ${this.emitArgs()})`
+		return `${tabs(indent)},(${this.funcallIfLambda()}${this.leftHand.emit(0)} ${this.emitArgs(indent, true)})`
 	}
 }
 
@@ -50,25 +50,25 @@ export class NamedArgumentsFunctionCall extends FunctionCall {
 		return 'temp' + Math.floor(Math.random() * 100000)
 	}
 
-	emitArgs(quoted = false) {
+	emitArgs(indent: number, quoted = false) {
 		let ret = ''
 		for (const argName of this.namedArgumentsNames) {
 			const propAccess = new PropertyAccess(new Identifier(this.BindingName), new Identifier(argName))
 			const propertySelection = quoted ? propAccess.emitQuoted(0) : propAccess.emit(0)
-			ret += `:${argName} ${propertySelection} `
+			ret += `\n${tabs(indent)}:${argName} ${propertySelection}`
 		}
 		return ret
 	}
 
 	emit(indent: number) {
 		return this.WrappingLet.emitWithBody(indent, (indent) => {
-			return `${tabs(indent)}(${this.funcallIfLambda()}${this.leftHand.emit(0)} ${this.emitArgs()})`
+			return `${tabs(indent)}(${this.funcallIfLambda()}${this.leftHand.emit(0)} ${this.emitArgs(indent+1)})`
 		})
 	}
 
 	emitQuoted(indent: number) {
 		return this.WrappingLet.emitWithBody(indent, (indent) => {
-			return `${tabs(indent)},(${this.funcallIfLambda()}${this.leftHand.emit(0)} ${this.emitArgs(true)})`
-		})
+			return `${tabs(indent)}(${this.funcallIfLambda()}${this.leftHand.emit(0)} ${this.emitArgs(indent+1)})`
+		}, true)
 	}
 }

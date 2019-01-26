e8a799942fed5a79d5259ee417fc551539c1207c
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2019-01-26 14:29:26 +0100

    Trying to deploy github release using travis

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-pushd tsce
+pushd ./tsce
 tsc
 popd
 node tsce/dist/index.js ./tests/tsconfig.json
-pushd tests/dist
+pushd ./tests/dist
 emacs --debug-init --batch --directory ./ --script ./tests.el -f ert-run-tests-batch-and-exit
 popd

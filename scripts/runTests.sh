a99d9dfeeb75a48ef0818c76b8d5dad9e02c8945
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2019-01-26 14:37:18 +0100

    Trying to fix script paths

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -5,7 +5,7 @@ set -e
 pushd ../tsce
 tsc
 popd
-node tsce/dist/index.js ./tests/tsconfig.json
+node ../tsce/dist/index.js ../tests/tsconfig.json
 pushd ../tests/dist
 emacs --debug-init --batch --directory ./ --script ./tests.el -f ert-run-tests-batch-and-exit
 popd

faf95cdb33b5440952f72514bd974bc2917ae4e8
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2019-01-26 02:22:59 +0100

    made sure to compile as a first step in running the tests

--- runTests.sh
+++ runTests.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+pushd tsce
+tsc
+popd
 node tsce/dist/index.js ./tests/tsconfig.json
 pushd ./tests/dist
 emacs --debug-init --batch --directory ./ --script ./tests.el -f ert-run-tests-batch-and-exit

5ed0900303e4ee2f9f0d7fff21b516dbb50d92d2
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2019-02-14 18:55:04 +0100

    Compiling assignment

--- tests/src/otherFileTests.ts
+++ tests/src/otherFileTests.ts
@@ -4,6 +4,9 @@ import { should, equal } from 'ert'
 export function testFromAnotherFileUsingTheEmacsApi() {
 	const foo = "bar"
 	const l = emacs.length(foo)
+	let test: number | string = 123
+	test = 321
+	test = "heisann"
 	should(equal(l, 3))
 	return true
 }

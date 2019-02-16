668dfe1360784cd2c37020f099a344d0d99816b4
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2019-02-16 15:10:37 +0100

    Brought back tests and compiling boolean literals

--- tests/src/otherFileTests.ts
+++ tests/src/otherFileTests.ts
@@ -1,19 +1,9 @@
 import * as emacs from "emacs"
 import { should, equal } from 'ert'
 
-
-export function testEverything() {
-	let foo = 10
-	let bar = 20
-	const test = bar + foo
-	const test2 = "foo"
-
-	if (test > 29) {
-		bar = 123
-	} else {
-		foo = 321
-	}
-
-	return foo
-
+export function testFromAnotherFileUsingTheEmacsApi() {
+	const foo = "bar"
+	const l = emacs.length(foo)
+	should(equal(l, 3))
+	return true
 }

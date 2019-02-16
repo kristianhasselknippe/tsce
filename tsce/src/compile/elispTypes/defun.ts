d3b581a2e0f43e4939028c8a7d14599f1e094e3f
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2019-02-16 20:37:48 +0100

    Compiling nested functions

--- tsce/src/compile/elispTypes/defun.ts
+++ tsce/src/compile/elispTypes/defun.ts
@@ -1,72 +1,78 @@
-import { Block, Node, tabs, hyphenate, Identifier } from "./";
-import { CompilerDirective } from "./compilerDirective";
+import { Block, Node, tabs, hyphenate, Identifier } from './';
+import { CompilerDirective } from './compilerDirective';
 
 export class FunctionArg extends Node {
-	type: string = "FunctionArg"
+	type: string = 'FunctionArg';
 
 	constructor(readonly name: Identifier) {
-		super()
+		super();
 	}
 
 	emit(indent: number) {
-		return this.name.emit(indent)
+		return this.name.emit(indent);
 	}
 }
 
 export class Defun extends Block {
 	type = 'Function';
 
-	private customForm?: string
-	private isInteractive?: boolean
+	private customForm?: string;
+	private isInteractive?: boolean;
 
-	constructor(identifier: Identifier, readonly args: FunctionArg[], readonly compilerDirectives?: CompilerDirective[]) {
+	constructor(
+		identifier: Identifier,
+		readonly args: FunctionArg[],
+		readonly compilerDirectives?: CompilerDirective[]
+	) {
 		super(identifier);
 
 		if (compilerDirectives && compilerDirectives.length > 0) {
 			for (const compDir of compilerDirectives) {
 				switch (compDir.kind) {
-					case "Form":
-						this.customForm = compDir.form
-						break
-					case "Interactive":
-						this.isInteractive = true
-						break
+					case 'Form':
+						this.customForm = compDir.form;
+						break;
+					case 'Interactive':
+						this.isInteractive = true;
+						break;
 				}
 			}
 		}
 	}
 
 	matchesIdentifier(identifierName: string) {
-		return this.identifier.identifierName === identifierName
+		return this.identifier.identifierName === identifierName;
 	}
 
 	isFunctionDeclaration() {
-		return true
+		return true;
 	}
 
 	emitArgs() {
-		return this.args.reduce((prev, curr) => prev + " " + curr.emit(0), "")
+		return this.args.reduce((prev, curr) => prev + ' ' + curr.emit(0), '');
 	}
 
 	getForm() {
 		if (this.customForm) {
-			return this.customForm
+			return this.customForm;
 		} else {
-			return "cl-defun"
+			return 'cl-defun';
 		}
 	}
 
 	emitInteractive(indent: number) {
 		if (this.isInteractive) {
-			return "\n" + tabs(indent) + "(interactive)"
+			return '\n' + tabs(indent) + '(interactive)';
 		} else {
-			return ""
+			return '';
 		}
 	}
 
 	emit(indent: number) {
-		return `${tabs(indent)}(${this.getForm()} ${this.identifier.emit(0)} (${this.emitArgs()})${this.emitInteractive(indent+1)}
-${this.emitBlock(indent + 1, this.emitBody(indent+2))}
-${tabs(indent)})`
+		return `${tabs(indent)}(${this.getForm()} ${this.identifier.emit(
+			0
+		)} (${this.emitArgs()})${this.emitInteractive(indent + 1)}
+${this.emitBlock(indent + 1, this.emitBody(indent + 2))}
+${tabs(indent)})`;
 	}
 }
